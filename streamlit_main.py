b4d10e5af774ff82d240bf185d1f3c70d0af68a4
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-04-16 08:52:06 +0200

    streamlit_patch_30_aaaa

--- streamlit_main.py
+++ streamlit_main.py
@@ -9,7 +9,7 @@ def predict_target(model, filename):
     img = tf.image.decode_image(img, channels=1)
     im = tf.cast(tf.image.resize(img, size=[32, 32]), dtype=tf.float32)
     pred = tf.squeeze(model.predict(tf.expand_dims(im, axis=0)))
-    return ImageOps.invert(pred.numpy())
+    return ~pred.numpy()
 
 
 def main():
